3fa4c22648047c79235edf08cac03c0f6e1a474d
Omotiafe <omotiafearegbeyen@gmail.com>
2024-04-11 22:55:02 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import image from "./images/icon-dice.svg";
+import pattern from "./images/pattern-divider-desktop.svg";
+import axios from "axios"
 
 function App() {
+  const [number, getNumber] = useState(0);
+  const [advice, getAdvice] = useState([]);
+
+  async function getData(){
+    const resp = await axios.get("https://api.adviceslip.com/advice");
+    // console.log(resp.data)
+    console.log(resp.data.slip.advice)
+
+    getAdvice(resp.data.slip.advice)
+    getNumber(resp.data.slip.id)
+  }
+
+  async function update(){
+    getData()
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="body">
+        <div className="parent">
+          <div className="child1">
+            <div className="header">ADVICE #{number}</div>
+            <div className="advice">"{advice}"</div>
+            <div>
+              <img src={pattern} alt="img" className="divider" />
+            </div>
+          </div>
+
+          <div className="dice-cont" onClick={() => update()}>
+            <img src={image} alt="img" className="dice" />
+          </div>
+        </div>
+      </div>
+
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by <a href="https://github.com/dashboard">Omotiafe Aregbeyen</a>
+        .
+      </div>
     </div>
   );
 }
